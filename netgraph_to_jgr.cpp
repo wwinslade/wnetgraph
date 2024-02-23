64a6148f542dd43e44c840fafa36f60fc035d647
wwinslade <103067129+wwinslade@users.noreply.github.com>
2024-02-23 10:25:09 -0500

    started driver cpp file

--- netgraph_to_jgr.cpp
+++ netgraph_to_jgr.cpp
@@ -1,71 +1,34 @@
 #include <iostream>
-#include <cstdio>
 #include <vector>
+#include <string>
 #include <fstream>
-#include <sstream>
 
 using namespace std;
 
-class Network {
+class Node {
   public:
-    Network(int argc, char **argv);
+    string hostname;
+    int layer;
+    vector < class Node * > children;
+    vector < class Node * > parents;
 
-  private:
-    string filename;
-    ifstream fin;
-    
-    int validate_file(char *fn);
-    
 };
 
-Network::Network(int argc, char **argv) {
-  // Variables
-  string line_in;
-  istringstream line_ss;
-  
-  // Input validation
-  try {
-    validate_file(argv[1]);
-  } catch (const exception &e) {
-    cerr << "Error: " << e.what() << endl;
-    throw;
-  }
-
-  // File is good, proceed with reading in input
-  while (getline(fin, line_in)) {
-    if (line_in.size() == 0) continue; // Line is empty, ignore it
-    
-    string w1;
-
-    line_ss.clear();
-    line_ss.str(line_in);
-
-    line_ss >> w1;
-    if (w1 == "//") continue; // Line is a comment or is empty, ignore it
-
-    if (w1 == "NETWORK") {
-
-    } else if (w1 == "SWITCH") {
-      
-    }
-
-  }
-};
-
-int Network::validate_file(char *fn) {
-  filename = fn;
-
-  fin.open(filename);
-  if (!fin.is_open()) throw runtime_error("File not found or could not be opened");
-
-  if (filename.size() < 5 || filename.substr(filename.size() - 5) != ".netg") {
-    throw invalid_argument("Invalid file extension. File must be of type .netg");
-  }
+class Graph {
+  public:
+    Node *root;
+    vector < Node * > all_nodes;
 };
 
 int main(int argc, char **argv) {
-  Network NET(argc, argv);
+  // Variables
+  ifstream fin;
+  ofstream fout;
   
+  try {
 
-  return 0;
+  } catch (const exception &e) {
+    fprintf(stderr, "%s\n", e.what());
+    exit(1);
+  }
 }
